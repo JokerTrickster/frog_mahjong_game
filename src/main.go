17230d1b38cebfaa998b4216f02bcb509ee19ecc
RyanBreathings <ryan@breathings.co.kr>
2023-11-16 22:09:56 +0900

    {add} - middleware init

--- src/main.go
+++ src/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	swaggerDocs "main/docs"
+	"main/middleware"
 	"net/http"
 	"os"
 
@@ -9,12 +11,17 @@ import (
 	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
+//export PATH=$PATH:~/go/bin
 func main() {
 	port := os.Getenv("APP_PORT")
 	if port == "" {
 		port = "3000" // 기본 포트 번호
 	}
 	e := echo.New()
+	if err := middleware.InitMiddleware(e); err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!!!!!!!")
