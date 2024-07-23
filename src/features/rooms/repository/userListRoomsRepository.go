08fb8226ccb0eefa0ce80e8c32f79c94c4a54e77
JokerTrickster <pkjhj485@gmail.com>
2024-07-23 17:53:28 +0900

    {fix} - 룸 유저 정보 가져오는 로직 수정

--- src/features/rooms/repository/userListRoomsRepository.go
+++ src/features/rooms/repository/userListRoomsRepository.go
@@ -19,7 +19,7 @@ func (d *UserListRoomsRepository) FindRoomUser(ctx context.Context, RoomsID uint
 	err := d.GormDB.Table("users").
 		Joins("LEFT JOIN room_users ON users.id = room_users.user_id").
 		Select("users.id AS user_id, room_users.id AS room_user_id, room_users.player_state, room_users.turn_number, room_users.owned_card_count, room_users.Room_id, room_users.score, users.name AS user_name, users.email AS user_email").
-		Where("room_users.room_id = ?", RoomsID).
+		Where("room_users.room_id = ? and room_users.deleted_at IS NULL", RoomsID).
 		Scan(&RoomUserList).Error
 	if err != nil {
 		return nil, utils.ErrorMsg(ctx, utils.ErrInternalDB, utils.Trace(), err.Error(), utils.ErrFromMysqlDB)
