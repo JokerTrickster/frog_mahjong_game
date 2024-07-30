9a9650952e5cfad4c1697c5915c71d994c7b0cdc
JokerTrickster <pkjhj485@gmail.com>
2024-07-31 02:01:34 +0900

    {feat} - ready cancel 이벤트 웹소켓 구현 \n #173

--- src/features/ws/websocket.go
+++ src/features/ws/websocket.go
@@ -18,25 +18,8 @@ func WSHandleMessages() {
 			CloseEventWebsocket(&msg)
 		case "READY": // 게임 준비
 			ReadyEventWebsocket(&msg)
-		case "CANCEL": // 게임 준비를 취소
-			//유저 정보를 ready로 변경
-			// 모든 플레이어가 ready였다가 취소한거라면 방장에게 게임 시작 불가능 메시지 전송
-
-			if clients, ok := entity.WSClients[msg.RoomID]; ok {
-				for client := range clients {
-					if msg.UserID == clients[client].UserID {
-						msg.Message = "준비 취소"
-					} else {
-						msg.Message = "준비를 취소 했습니다."
-					}
-					err := client.WriteJSON(msg)
-					if err != nil {
-						log.Printf("error: %v", err)
-						client.Close()
-						delete(clients, client)
-					}
-				}
-			}
+		case "READY_CANCEL": // 게임 준비를 취소
+			ReadyCancelEventWebsocket(&msg)
 		case "START": // 게임 시작
 			// 모든 플레이어에게 게임 시작을 알리고
 			// 게임 준비를 한다.
