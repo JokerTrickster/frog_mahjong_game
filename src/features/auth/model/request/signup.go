6d5185b774f76ef111db6535a1060166079b52f7
JokerTrickster <pkjhj485@gmail.com>
2024-08-27 17:05:55 +0900

    {improvement} - 유저 정보 길이 제한 \n #240

--- src/features/auth/model/request/signup.go
+++ src/features/auth/model/request/signup.go
@@ -2,7 +2,7 @@ package request
 
 // 닉네임, 비밀번호, 이메일 정도만 정보를 받는다.
 type ReqSignup struct {
-	Name     string `json:"name"`
-	Password string `json:"password"`
-	Email    string `json:"email"`
+	Name     string `json:"name" validate:"min=2,max=6"`
+	Password string `json:"password" validate:"min=6,max=10"`
+	Email    string `json:"email" validate:"email"`
 }
