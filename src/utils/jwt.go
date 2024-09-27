4110b883b427469cd7ed9361f80590aff243e1cf
JokerTrickster <pkjhj485@gmail.com>
2024-09-27 23:53:15 +0900

    {test} - 토큰 만료시간 30초로 변경

--- src/utils/jwt.go
+++ src/utils/jwt.go
@@ -22,7 +22,7 @@ var RefreshTokenSecretKey []byte
 var JwtConfig middleware.JWTConfig
 
 const (
-	AccessTokenExpiredTime  = 3          //hours
+	AccessTokenExpiredTime  = 30         //hours
 	RefreshTokenExpiredTime = 1 * 24 * 7 //hours
 )
 
@@ -48,7 +48,7 @@ func GenerateToken(email string, userID uint) (string, int64, string, int64, err
 
 func GenerateAccessToken(email string, now time.Time, userID uint) (string, int64, error) {
 	// Set custom claims
-	expiredAt := now.Add(time.Minute * AccessTokenExpiredTime).Unix()
+	expiredAt := now.Add(time.Second * AccessTokenExpiredTime).Unix()
 	claims := &JwtCustomClaims{
 		TimeToEpochMillis(now),
 		userID,
