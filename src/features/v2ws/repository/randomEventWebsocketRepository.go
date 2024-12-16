f9b658392896aecdccc050ffd7460d80cca989be
JokerTrickster <pkjhj485@gmail.com>
2024-12-16 15:10:30 +0900

    {fix} - 랜덤으로 여러개 가지고 갔을때 소유 카드 수 제대로 반영하도록 개선 \n

--- src/features/v2ws/repository/randomEventWebsocketRepository.go
+++ src/features/v2ws/repository/randomEventWebsocketRepository.go
@@ -63,8 +63,9 @@ func RandomCreateCard(c context.Context, tx *gorm.DB, userBirdCardDTO *mysql.Use
 }
 
 func RandomUpdateRoomUserCardCount(c context.Context, tx *gorm.DB, entity *entity.WSRandomEntity) error {
+	addedCount := fmt.Sprintf("owned_card_count + %d", entity.Count)
 	// 유저id로 room_users에서 찾아서 card_count를 더한 후 업데이트 한다.
-	err := tx.Clauses(clause.Locking{Strength: "UPDATE"}).Model(&mysql.RoomUsers{}).Where("room_id = ? AND user_id = ?", entity.RoomID, entity.UserID).Update("owned_card_count", gorm.Expr("owned_card_count + 1")).Error
+	err := tx.Clauses(clause.Locking{Strength: "UPDATE"}).Model(&mysql.RoomUsers{}).Where("room_id = ? AND user_id = ?", entity.RoomID, entity.UserID).Update("owned_card_count", gorm.Expr(addedCount)).Error
 	if err != nil {
 		return fmt.Errorf("방 유저 카드 카운트 업데이트 실패 %v", err.Error())
 	}
